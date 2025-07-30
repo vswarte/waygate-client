1752c132787fffd2a23e7a2cd2a01ed162e3c5e1
FeeeeK <sign@axd1x8a.me>
2025-07-30 07:55:36 +0300

    Fix packet queue not being cleared on disconnect

--- src/p2p/queue.rs
+++ src/p2p/queue.rs
@@ -27,4 +27,8 @@ impl GamePacketQueue {
     pub fn pop(&self, remote: u64, packet_type: u8) -> Option<(u8, Vec<u8>)> {
         self.inbound.entry(remote).or_default().0[packet_type as usize].pop_front()
     }
+
+    pub fn remove(&self, remote: u64) -> Option<PlayerGamePacketQueue> {
+        self.inbound.remove(&remote).map(|entry| entry.1)
+    }
 }
